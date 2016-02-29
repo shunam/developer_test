15e275301e26c18eec6600f7a27dfdb4eb69ce18
Hendrik Kustanto <hendrik@shunam.com>
2016-02-29 20:57:32 +0700

    Menambahkan test untuk sorting by id

--- test_find_student.rb
+++ test_find_student.rb
@@ -12,4 +12,18 @@ class TestFindStudent < Minitest::Test
     find_student = FindStudent.new(3)
     assert_equal 39, find_student.calculate_students_distance.count
   end
+
+  def test_sorted_by_id
+    find_student = FindStudent.new(1)
+    assert_equal 133, find_student.calculate_students_distance[0][0]
+    assert_equal 238, find_student.calculate_students_distance[1][0]
+    assert_equal 295, find_student.calculate_students_distance[2][0]
+    assert_equal 690, find_student.calculate_students_distance[3][0]
+
+    find_student = FindStudent.new(2)
+    assert_equal 35, find_student.calculate_students_distance[0][0]
+    assert_equal 81, find_student.calculate_students_distance[1][0]
+    assert_equal 133, find_student.calculate_students_distance[2][0]
+    assert_equal 198, find_student.calculate_students_distance[3][0]
+  end
 end
