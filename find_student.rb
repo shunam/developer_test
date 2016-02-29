7b7a1213e90319095ecba2e3cbaa008d0e3fc643
Hendrik Kustanto <hendrik@shunam.com>
2016-02-29 15:25:17 +0700

    - Implement perhitungan jarak dari point A ke point B

--- find_student.rb
+++ find_student.rb
@@ -1,3 +1,14 @@
 require './great_circle'
+require 'csv'
 
-puts GreatCircle.distance(51.507222, -0.1275, 48.8567, 2.3508)
+puts "Kirim invitation dalam jarak ? (km)"
+distance = gets.chomp.to_i
+gandaria_lat = -6.243376
+gandaria_long = 106.784425
+
+CSV.foreach("./students.csv") do |row|
+  temp_distance = GreatCircle.distance(gandaria_lat, gandaria_long, row[2].to_f, row[3].to_f)
+  if temp_distance < distance
+    puts "JARAK: #{temp_distance} km, \tID: #{row[0]}, \tNAME: #{row[1]}"
+  end
+end
